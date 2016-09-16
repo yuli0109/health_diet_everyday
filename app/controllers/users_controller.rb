a8951e2fddf49e4e3621fd34e2ba09d7498c8bac
Yuli Zhong <yz924@cornell.edu>
2016-09-16 11:53:29 -0700

    fix controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render 'new'
+    end
+
   end
-
-
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
