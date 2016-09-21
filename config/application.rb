56840e59cbd85c99a86c40e04c411e5c298db977
Yuli Zhong <yz924@cornell.edu>
2016-09-21 11:51:04 -0700

    font

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,9 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module HealthDietEveryday
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
